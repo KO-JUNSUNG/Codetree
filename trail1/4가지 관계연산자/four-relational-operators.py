212de8c27331ad30415454a16d70857840c67378
KO-JUNSUNG <57568385+KO-JUNSUNG@users.noreply.github.com>
2026-08-19 20:10:50 +0900

    [Passed] 4가지 관계연산자 (51ms, 16MB)

---
+++ trail1/4가지 관계연산자/four-relational-operators.py
@@ -0,0 +1,12 @@
+def boolean_(condition):
+    if condition == True:
+        print(1)
+    else:
+        print(0)
+
+A,B = map(int,input().split())
+boolean_(A>=B)
+boolean_(A>B)
+
+boolean_(B>=A)
+boolean_(B>A)
