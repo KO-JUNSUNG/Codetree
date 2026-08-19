0a2041836e69c5b19e18ea49465ddad935f5e467
KO-JUNSUNG <57568385+KO-JUNSUNG@users.noreply.github.com>
2026-08-19 19:41:23 +0900

    [Passed] 알파벳에 따른 평가 (47ms, 16MB)

---
+++ trail1/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,13 @@
+chr = input().upper()
+if chr == 'S':
+    print("Superior")
+elif chr == 'A':
+    print("Excellent")
+elif chr == 'B':
+    print("Good")
+elif chr == 'C':
+    print("Usually")
+elif chr == 'D':
+    print("Effort")
+else:
+    print("Failure")
