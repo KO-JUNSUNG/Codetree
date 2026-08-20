bad38dd5d12478c65c19a10e91702e40f468ad83
KO-JUNSUNG <57568385+KO-JUNSUNG@users.noreply.github.com>
2026-08-20 15:08:26 +0900

    [Passed] 수의 계절은 (48ms, 16MB)

---
+++ trail1/수의 계절은/season-of-num.py
@@ -0,0 +1,14 @@
+M = int(input())
+spring = [3,4,5]
+summer = [6,7,8]
+fall = [9,10,11]
+winter = [12,1,2]
+
+if M in spring:
+    print("Spring")
+if M in summer:
+    print("Summer")
+if M in fall:
+    print("Fall")
+if M in winter:
+    print("Winter")
